bf0f98ec55f64cea9ab6353261ebb240f937f05a
Alvaro Salvino <alvaro.salvino@gmail.com>
2024-03-17 18:41:14 -0300

    .

--- app.py
+++ app.py
@@ -2,84 +2,53 @@ import json
 import os
 import folium
 #import webview
-from models.repository.usercolecao_repository import usercolecaoRepository
-from models.connection_options.connection import DBConnectionHandler
-from folium.plugins import TagFilterButton, FastMarkerCluster
+from folium.plugins import TagFilterButton
+from geopy.geocoders import Bing
 from pymongo import MongoClient
-from flask import Flask, redirect, request, flash, render_template, render_template_string
-from selenium.webdriver.common.by import By
-
-
-
+from flask import Flask, redirect, request, flash, render_template, render_template_string, session
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Ti-Malta"
+client = MongoClient('mongodb+srv://alvarosalvino:gruPzOlFAW1BfADK@imap.aznrbqq.mongodb.net/?retryWrites=true&w=majority&appName=imap')
+db = client['imapdb']
+collection = db['usercolecao']
 
 #window = webview.create_window('Mapa de Polos Malta', app)
 
-
-
-
-
-arquivos = os.listdir()
 logado = False
 
 
 @app.route('/')
 def home():
-    global logado
-    logado = False
+    session['logado'] = False
     return render_template("login.html")
+
 
 @app.route('/user')
 def user():
-    if os.path.exists("C:\\Polos_Malta_Map\\output\\Mapa Polos Malta\\mapas.json"):
-        global logado
-        if logado == True:
-            return render_template("usuario.html")
-        if logado == False:
-            return redirect('/')
+    if 'logado' in session and session['logado'] == True:
+        return render_template("usuario.html")
+    else:
+        return redirect('/')
 
 @app.route('/mapolo')
 def mapolo():
-    global logado
-    if logado == True:
-        polo = []
-        parceiro = []
-        parceiro_local = []
-        endereco = []
-        keys = ('polo', 'parceiro', 'parceiro_local', 'endereco')
-        polos_coordenadas = []
-        with open("C:\\Polos_Malta_Map\\output\\Mapa Polos Malta\\mapas.json") as file:
-            data = json.load(file)
-            for i in data:
-                polos_coordenadas.append({key: i[key] for key in keys})
-                polo.append('polo')
-                parceiro.append('parceiro')
-                parceiro_local.append('parceiro_local')
-                endereco.append('endereco')
-        for polo_coordenada in polos_coordenadas:
-            latitude, longitude = polo_coordenada['endereco'].split("{")[-1].split("{")[0].split()
-            ipolo = polo_coordenada['polo']
-            iparceiro = polo_coordenada['parceiro']
-            iparceiro_local = polo_coordenada['parceiro_local']
-            polo_coordenada['ipolo'] = str(ipolo)
-            polo_coordenada['iparceiro'] = str(iparceiro)
-            polo_coordenada['iparceiro_local'] = str(iparceiro_local)
-            polo_coordenada['latitude'] = float(latitude)
-            polo_coordenada['longitude'] = float(longitude)
-            polo_coordenada['popup'] = str(ipolo+' '+iparceiro+' '+iparceiro_local)
+    if 'logado' in session and session['logado'] == True:
+        polos_coordenadas = list(db['mapacolecao'].find())
         polos_mapa = folium.Map(location=[-5.1188834018636795, -42.803002102827975], zoom_start=5, control_scale=True,
                                 world_copy_jump=True, no_wrap=True)
         folium.LayerControl().add_to(polos_mapa)
+        tags = []
         for polo_coordenada in polos_coordenadas:
-            coordenadas = (polo_coordenada['latitude'], polo_coordenada['longitude'])
-            marcador = coordenadas
-            folium.Marker(coordenadas, popup=polo_coordenada['popup'], tags=[marcador]).add_to(polos_mapa)
+            latitude, longitude = map(float, polo_coordenada['endereco'].split())
+            coordenadas = (latitude, longitude)
+            polo_coordenada['popup'] = str(polo_coordenada['polo']+' '+polo_coordenada['parceiro']+' '+polo_coordenada['parceiro_local'])
+            tags.extend([polo_coordenada['polo'], polo_coordenada['parceiro'], polo_coordenada['parceiro_local']])
+            folium.Marker(coordenadas, popup=polo_coordenada['popup'], tags=[polo_coordenada['polo'], polo_coordenada['parceiro'], polo_coordenada['parceiro_local']]).add_to(polos_mapa)
 
-
-        TagFilterButton(marcador).add_to(polos_mapa)
+        tags.sort()  # Ordena as tags em ordem alfabética
+        TagFilterButton(tags).add_to(polos_mapa)
         polos_mapa.get_root().render()
         header = polos_mapa.get_root().header.render()
         body_html = polos_mapa.get_root().html.render()
@@ -105,65 +74,63 @@ def mapolo():
             script=script,
         )
 
-    if logado == False:
+    else:
         return redirect('/')
 
-    # polos_mapa.add_child(folium.LatLngPopup()
 
 @app.route("/login", methods=['POST'])
 def login():
-
-    global logado
     usuario = request.form.get('nome')
     senha = request.form.get('senha')
-    with open("C:\\Polos_Malta_Map\\output\\Mapa polos Malta\\users.json") as usuarios:
-        lista = json.load(usuarios)
-        cont = 0
-        for c in lista:
-            cont+=1
-            if usuario == c['nome'] and senha == c['senha']:
-                logado = True
-                return redirect('/user')
-            if cont >= len(lista):
-                flash('Usuáio Inválido')
-                return redirect("/")
+    usercolecao = collection.find_one({'nome': usuario, 'senha': senha})
+    print(collection.find_one())
+    if usercolecao:
+        session['logado'] = True
+        return redirect('/user')
+    else:
+        flash('Usuário Inválido')
+        return redirect("/")
 
-@app.route("/inserirpolo")
+@app.route("/inserirpolo", methods=['GET', 'POST'])
 def inserirpolo():
-    global logado
-    if logado == True:
-        return render_template("novopolo.html")
-    if logado == False:
+    if 'logado' in session and session['logado'] == True:
+        if request.method == 'POST':
+            polo = request.form.get('polo')
+            parceiro = request.form.get('parceiro')
+            parceiro_local = request.form.get('parceiro_local')
+            endereco = request.form.get('endereco')
+            novo_polo = {"polo": polo, "parceiro": parceiro, "parceiro_local": parceiro_local, "endereco": endereco}
+            db['mapacolecao'].insert_one(novo_polo)
+            return redirect('/user')
+        else:
+            return render_template("novopolo.html")
+    else:
         return redirect('/')
 
-@app.route("/novopolo", methods=['POST'])
+
+@app.route("/novopolo", methods=['GET', 'POST'])
 def insere_instituicao():
-    global logado
-    if logado == True:
-        polos = []
-        polo = request.form.get('polo')
-        parceiro = request.form.get('parceiro')
-        parceiro_local = request.form.get('parceiro_local')
-        endereco = request.form.get('endereco')
-        polos = [
-            {
+    if 'logado' in session and session['logado'] == True:
+        if request.method == 'POST':
+            geolocator = Bing(api_key="Al-_22HyOgNBpnd_1y9JqJB82qwqya-LHAfkvtszL6HT6gUuxc8C-Khq9whz63IQ")
+            polo = request.form.get('polo')
+            parceiro = request.form.get('parceiro')
+            parceiro_local = request.form.get('parceiro_local')
+            cidade = request.form.get('cidade')
+            location = geolocator.geocode(cidade)
+            endereco = f"{location.latitude} {location.longitude}"
+            novo_polo = {
                 "polo": polo,
                 "parceiro": parceiro,
                 "parceiro_local": parceiro_local,
                 "endereco": endereco
             }
-        ]
-        with open("C:\\Polos_Malta_Map\\output\\Mapa Polos Malta\\mapas.json") as mapas:
-            mapa = json.load(mapas)
-
-        mapaNovo = mapa + polos
-
-        with open("C:\\Polos_Malta_Map\\output\\Mapa Polos Malta\\mapas.json", 'w') as gravarMapas:
-            json.dump(mapaNovo, gravarMapas, indent=4)
+            db['mapacolecao'].insert_one(novo_polo)
             flash('Inserção bem sucedida')
         return render_template("novopolo.html")
-    if logado == False:
+    else:
         return redirect('/')
+
 
 
 if __name__ == '__main__':
--- models/connection_options/mongo_db_configs.py
+++
@@ -1,7 +0,0 @@
-mongo_db_infos = {
-    "HOST": "ac-ditbxoz-shard-00-01.aznrbqq.mongodb.net",
-    "PORT": "27017",
-    "USERNAME": "alvarosalvino",
-    "PASSWORD": "gruPzOlFAW1BfADK",
-    "DB_NAME": "imapdb"
-}
